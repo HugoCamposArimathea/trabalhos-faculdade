d52236cee2cc8be6db5ab7e7684572d1e8c4d186
Hugo Campos <hugocamposarimathea@gmail.com>
2023-10-20 21:35:20 -0300

    feat: Primeiro commit, adicionando trabalho ao Git

---
+++ 2023-54-linguagemC-AE1/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main()
+{
+
+    setlocale(LC_ALL, "");
+
+    float valorInvestido = 0;
+    float percentualRendimento = 0;
+    float resultado;
+    float montante;
+    int periodo = 0;
+
+    printf("Digite o valor investido\n");
+    scanf("%f", &valorInvestido);
+    printf("Digite a quantidade de meses do investimento\n");
+    scanf("%d", &periodo);
+    printf("Digite o percentual de rendimento ao mês\n");
+    scanf("%f", &percentualRendimento);
+
+    montante = valorInvestido; 
+
+    for (int i = 0; i < periodo; i++)
+    {
+        float taxa = percentualRendimento/100;
+        resultado = valorInvestido*(1+taxa);
+        valorInvestido = resultado;
+        printf("O rendimento foi: R$%.2f\n", resultado);
+    }
+
+    printf("\n");
+    printf("O rendimento do seu investimento antes dos impostos ficou em: R$%.2f\n", resultado);
+    printf("\n");
+    printf("E necesario pagar 15 de imposto sobre o rendimento do seu investimento. Dessa forma, o resultado final do seu investimento ficou assim:");
+    printf("\n\n");
+
+    float rendimento = valorInvestido-montante;
+    float imposto = rendimento*0.15;
+    float resultadoFinal = valorInvestido-imposto;
+    printf("R$%.2f\n", resultadoFinal);
+
+}
