eb8c8a401e62be696ea4312a8eb8b92bd774778e
Hugo Campos <hugocamposarimathea@gmail.com>
2023-11-14 21:42:39 -0300

    feat: Inicialização da função de validação da meta

--- 2023-54-linguagemC-AE3/main.c
+++ 2023-54-linguagemC-AE3/main.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 
 void validarMeta(int dia1[3][3], int dia2[3][3]){
+    int meta;
+    int matrizTotal[3][3];
+    int i, j = 0;
+
+    //Leitura da meta por setor para verificação
+    printf("Digite qual é a meta por setor");
+    scanf("%d", &meta);
+
+    //Soma das duas matrizes
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            matrizTotal[i][j] = dia1[i][j] + dia2[i][j];
+        }
+    }
 
 }
 
@@ -17,6 +31,8 @@ int main() {
             scanf("%d", &dia1[i][j]);
         }
     }
+
+    printf("\n");
 
     //Leitura da matriz no segundo dia
     for(i=0; i<3; i++){
