31730ceb25214d509a25007c49668fd70446ad0e
Hugo Campos <hugocamposarimathea@gmail.com>
2023-11-14 22:13:26 -0300

    feat: Função de soma das matrizes

--- 2023-54-linguagemC-AE3/main.c
+++ 2023-54-linguagemC-AE3/main.c
@@ -7,8 +7,10 @@ void validarMeta(int dia1[3][3], int dia2[3][3]){
     int i, j = 0;
 
     //Leitura da meta por setor para verificação
-    printf("Digite qual é a meta por setor");
+    printf("Digite qual e a meta por setor \n");
     scanf("%d", &meta);
+
+    printf("\n");
 
     //Soma das duas matrizes
     for(i=0; i<3; i++){
@@ -16,7 +18,15 @@ void validarMeta(int dia1[3][3], int dia2[3][3]){
             matrizTotal[i][j] = dia1[i][j] + dia2[i][j];
         }
     }
-
+    
+    //Escrita da matriz total na tela
+    printf("A matriz total e: \n");
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            printf("%d ", matrizTotal[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main() {
@@ -40,6 +50,8 @@ int main() {
             printf("Digite o total de pessoas no setor %d de %d\n", (i+1), (j+1));
         }
     }
+
+    printf("\n");
 
     validarMeta(dia1, dia2);
 }
