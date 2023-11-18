b3f52891a7a41daf6e74635e82ae38534d515c54
Hugo Campos <hugocamposarimathea@gmail.com>
2023-11-17 22:03:28 -0300

    feat: Criação do menu inicial, inicialização da função de atendimento.

--- 2023-54-linguagemC-MAPA/main.c
+++ 2023-54-linguagemC-MAPA/main.c
@@ -2,22 +2,41 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void aberturaConta(){
+void solicitarAtend(){
+    char nome[50];
+    int cpf;
 
-}
-
-void caixa(){
-
-}
-
-void gerentePF(){
-
-}
-
-void gerentePJ(){
-
+    printf("Iremos dar prosseguimento ao seu atendimento. Mas antes, preciso que você digite o seu nome:\n");
+    scanf("%s", &nome);
+    printf("\n");
+    printf("Certo %s. Agora preciso do seu CPF:\n", nome);
+    scanf("%d", &cpf);
+    printf("\n");
 }
 
 int main(){
-    
+
+    setlocale(LC_ALL, "");
+
+    int escolha;
+
+    printf("Bem vindo ao sistema de atendimento!\n");
+    printf(" 1 - Solicitar Atendimento\n 2 - Listar Atendimentos Registrados\n 3 - Listar Atendimento por setor\n 4 - Sair");
+    printf("\n");
+    scanf("%d", &escolha);
+
+    switch (escolha){
+    case 1:
+        solicitarAtend();
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    default:
+        printf("Desculpe! Acho que você digitou algo errado.\n");
+        break;
+    }
 }
