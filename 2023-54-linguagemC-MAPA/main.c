089a04f0de3ac862118debc9e12321eafa62591a
Hugo Campos <hugocamposarimathea@gmail.com>
2023-11-17 21:24:19 -0300

    feat: Inicio de projeto, definição de sub-rotinas

---
+++ 2023-54-linguagemC-MAPA/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void aberturaConta(){
+
+}
+
+void caixa(){
+
+}
+
+void gerentePF(){
+
+}
+
+void gerentePJ(){
+
+}
+
+int main(){
+    
+}
