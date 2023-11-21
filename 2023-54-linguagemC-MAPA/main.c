fc6c4b7cd97c431c6d4477d5e5019eeda593287a
Hugo Campos <hugocamposarimathea@gmail.com>
2023-11-20 22:13:24 -0300

    feat: Conclusão da função de listagen de atendimentos realizados

--- 2023-54-linguagemC-MAPA/main.c
+++ 2023-54-linguagemC-MAPA/main.c
@@ -5,8 +5,9 @@
 //Struct para registro dos atendimentos 
 struct registroAtend {
     char nome_cliente[50];
-    int cpf_cliente[12];
-    char atend_cliente[50];
+    char sobrenome_cliente[50];
+    long long int cpf_cliente;
+    int atend_cliente; 
 };
 
 struct registroAtend Vet[10];
@@ -16,6 +17,7 @@ void menuPrincipal(){
 
     int escolha;
 
+    printf("\n\n");
     printf("Bem vindo ao sistema de atendimento!\n");
     printf(" 1 - Solicitar Atendimento\n 2 - Listar Atendimentos Registrados\n 3 - Listar Atendimento por setor\n 4 - Sair");
     printf("\n");
@@ -26,52 +28,60 @@ void menuPrincipal(){
         solicitarAtend();
         break;
     case 2:
+        solicitarAtendRegist();
         break;
     case 3:
         break;
     case 4:
         break;
     default:
-        printf("Desculpe! Acho que você digitou algo errado.\n");
+        printf("Desculpe! Acho que voce digitou algo errado.\n");
         menuPrincipal();
         break;
     }
 }
 
+//Sub-rotina para a escolha um do menu principal - Solicitar Atendimento
 void solicitarAtend(){
     int escolha2;
     int confirmacao = 1;
-    int i = 0;
+    int i = -1;
 
     while (confirmacao == 1){
 
-        printf("Iremos dar prosseguimento ao seu atendimento. Mas antes, preciso que você digite o seu nome:\n");
-        gets(Vet[i].nome_cliente);
+        i++;
+        printf("Iremos dar prosseguimento ao seu atendimento. Mas antes, preciso que voce digite o seu nome:\n");
+        fflush(stdin);
+        scanf("%s", &Vet[i].nome_cliente);
+        printf("\n");
+        printf("Agora o seu sobrenome:\n");
+        fflush(stdin);
+        scanf("%s", &Vet[i].sobrenome_cliente);
         printf("\n");
         printf("Certo. Agora preciso do seu CPF:\n");
-        scanf(Vet[i].cpf_cliente);
+        fflush(stdin);
+        scanf("%llu", &Vet[i].cpf_cliente);
         printf("\n");
-        printf("Agora preciso que você escolha uma das opções abaixo para darmos proseguimento a sua solicitação:\n 1 - Abertura de Conta\n 2 - Caixa\n 3 - Gerente Pessoa Física\n 4 - Gerente Pessoa Jurídica\n 5 - Voltar ao menu principal\n");
+        printf("Agora preciso que voce escolha uma das opcoes abaixo para darmos proseguimento a sua solicitacao:\n 1 - Abertura de Conta\n 2 - Caixa\n 3 - Gerente Pessoa Fisica\n 4 - Gerente Pessoa Juridica\n 5 - Voltar ao menu principal\n");
+        fflush(stdin);
         printf("\n");
-        scanf("%d", &escolha2, &Vet[i].atend_cliente);
-
-        i++;
+        scanf("%d", &Vet[i].atend_cliente, escolha2);
 
         switch (escolha2){
         case 1:
-            printf("Você deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Não)\n");
+            printf("Voce deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Nao)\n");
             scanf("%d", &confirmacao);
             break;
         case 2:
-            printf("Você deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Não)\n");
+            printf("Voce deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Nao)\n");
             scanf("%d", &confirmacao);
             break;
         case 3:
-            printf("Você deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Não)\n");
+            printf("Voce deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Nao)\n");
             scanf("%d", &confirmacao);
             break;
         case 4:
-            printf("Você deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Não)\n");
+            printf("Voce deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Nao)\n");
             scanf("%d", &confirmacao);
             break;
         case 5:
@@ -79,7 +89,7 @@ void solicitarAtend(){
         default:
             printf("Oops! Creio que tenha digitado algo errado.\n");
             printf("\n");
-            printf("Você deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Não)\n");
+            printf("Voce deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Nao)\n");
             scanf("%d", &confirmacao);
             break;
         }
@@ -87,8 +97,31 @@ void solicitarAtend(){
     menuPrincipal();
 }
 
+//Sub-rotina para os registros de atendimento
+void solicitarAtendRegist() {
+
+    printf("\n\n");
+    printf("-------------------------------------\n");
+    printf("Listagem dos atendimentos realizados:\n");
+    printf("-------------------------------------\n");
+
+    for (int i = 0; i < 10; i++)
+    {
+        if(Vet[i].cpf_cliente>0){
+            printf("\n");
+            printf("-------------------------------------\n");
+            printf("Nome: %s", Vet[i].nome_cliente);
+            printf(" %s\n", Vet[i].sobrenome_cliente);
+            printf("CPF: %llu\n", Vet[i].cpf_cliente);
+            printf("Tipo de Atendimento: %d\n", Vet[i].atend_cliente);
+        } else {
+            break;
+        }
+    }
+}
+
 int main(){
 
-    setlocale (LC_ALL, "portuguese");
+    setlocale (LC_ALL, "Portuguese");
     menuPrincipal();
 }
