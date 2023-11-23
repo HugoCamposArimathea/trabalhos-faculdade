151f53e7f8b6540c5188cbabc9f4d80452f068ed
Hugo Campos <hugocamposarimathea@gmail.com>
2023-11-23 20:24:10 -0300

    fix: Correção de bugs da listagem de atendimentos realizados

--- 2023-54-linguagemC-MAPA/main.c
+++ 2023-54-linguagemC-MAPA/main.c
@@ -6,6 +6,7 @@
 struct registroAtend {
     char nome_cliente[50];
     char sobrenome_cliente[50];
+    char atendEscolhido[50];
     long long int cpf_cliente;
     int atend_cliente; 
 };
@@ -33,6 +34,7 @@ void menuPrincipal(){
     case 3:
         break;
     case 4:
+        exit("int status");
         break;
     default:
         printf("Desculpe! Acho que voce digitou algo errado.\n");
@@ -42,47 +44,48 @@ void menuPrincipal(){
 }
 
 //Sub-rotina para a escolha um do menu principal - Solicitar Atendimento
+int j = -1;
 void solicitarAtend(){
     int escolha2;
     int confirmacao = 1;
-    int i = -1;
 
     while (confirmacao == 1){
 
-        i++;
+        j++;
         printf("Iremos dar prosseguimento ao seu atendimento. Mas antes, preciso que voce digite o seu nome:\n");
         fflush(stdin);
-        scanf("%s", &Vet[i].nome_cliente);
+        scanf("%s", &Vet[j].nome_cliente);
         printf("\n");
         printf("Agora o seu sobrenome:\n");
         fflush(stdin);
-        scanf("%s", &Vet[i].sobrenome_cliente);
+        scanf("%s", &Vet[j].sobrenome_cliente);
         printf("\n");
         printf("Certo. Agora preciso do seu CPF:\n");
         fflush(stdin);
-        scanf("%llu", &Vet[i].cpf_cliente);
+        scanf("%llu", &Vet[j].cpf_cliente);
         printf("\n");
         printf("Agora preciso que voce escolha uma das opcoes abaixo para darmos proseguimento a sua solicitacao:\n 1 - Abertura de Conta\n 2 - Caixa\n 3 - Gerente Pessoa Fisica\n 4 - Gerente Pessoa Juridica\n 5 - Voltar ao menu principal\n");
         fflush(stdin);
         printf("\n");
-        scanf("%d", &Vet[i].atend_cliente, escolha2);
+        scanf("%d", &escolha2);
+        Vet[j].atend_cliente = escolha2;
 
         switch (escolha2){
         case 1:
-            printf("Voce deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Nao)\n");
-            scanf("%d", &confirmacao);
+            printf("Você escolheu opção 1: Abertura de Conta. Muito obrigado! \n");
+            menuPrincipal();
             break;
         case 2:
-            printf("Voce deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Nao)\n");
-            scanf("%d", &confirmacao);
+            printf("Você escolheu opção 2: Caixa. Muito obrigado! \n");
+            menuPrincipal();
             break;
         case 3:
-            printf("Voce deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Nao)\n");
-            scanf("%d", &confirmacao);
+            printf("Você escolheu opção 3: Gerente de Pessoa Fisica. Muito obrigado! \n");
+            menuPrincipal();
             break;
         case 4:
-            printf("Voce deseja voltar ao menu para mais algum atendimento? (1-Sim/2-Nao)\n");
-            scanf("%d", &confirmacao);
+            printf("Você escolheu opção 4: Gerente Pessoa Juridica. Muito obrigado! \n");
+            menuPrincipal();
             break;
         case 5:
             menuPrincipal();
@@ -113,11 +116,20 @@ void solicitarAtendRegist() {
             printf("Nome: %s", Vet[i].nome_cliente);
             printf(" %s\n", Vet[i].sobrenome_cliente);
             printf("CPF: %llu\n", Vet[i].cpf_cliente);
-            printf("Tipo de Atendimento: %d\n", Vet[i].atend_cliente);
-        } else {
-            break;
-        }
+            if(Vet[i].atend_cliente == 1){
+                printf("Tipo de Atendimento: 1 - Abertura de Conta\n");
+            }else if(Vet[i].atend_cliente == 2){
+                printf("Tipo de Atendimento: 2 - Caixa\n");
+            }else if(Vet[i].atend_cliente == 3){
+                printf("Tipo de Atendimento: 3 - Gerente de Pessoa Fisica\n");
+            }else if(Vet[i].atend_cliente == 4){
+                printf("Tipo de Atendimento: 4 - Gerente de Pessoa Juridica\n");
+            }else if(Vet[i].atend_cliente == 5){
+                printf("Tipo de Atendimento: 5 - Voltar ao Menu Principal\n");
+            }
+        } 
     }
+    menuPrincipal();
 }
 
 int main(){
