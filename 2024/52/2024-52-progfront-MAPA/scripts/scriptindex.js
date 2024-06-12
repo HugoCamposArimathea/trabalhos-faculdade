b1ded28820ff082acd65922dfb25312cb94a14c8
HugoCamposArimathea <hugocamposarimathea@gmail.com>
2024-06-11 21:38:09 -0300

    Começo do JavaScript e correção do jQuery

---
+++ 2024/52/2024-52-progfront-MAPA/scripts/scriptindex.js
@@ -0,0 +1,14 @@
+//Código referente ao funcionamento dos botões 
+$(document).ready(function(){
+    $('#botao1').click(function(){
+        window.location.href = "loggin.html";
+    });
+
+    $('#botao2').click(function(){
+        window.location.href = "cadastro.html";
+    });
+
+    $('#botao3').click(function(){
+        window.location.href = "index.html";
+    });
+});
